19c2c0bde382e43a1491910aa08f7dcbac2395d0
Filip Kristek <filipkristek4@gmail.com>
2023-08-20 19:03:08 +0200

    Metoda Format
    Zkoušení metody format.
    Rozdíl formatovací řetězce x metoda format

--- retezce.py
+++ retezce.py
@@ -1,4 +1,18 @@
-jmeno = input("Napiš jméno: ")
-prijmeni = input("Napiš přijmení: ")
-inicialy = jmeno[0] + ". "+ prijmeni[0] +"."
-print("Inicialý jsou: " + inicialy.upper())
+y_a = "ý"
+osloveni = "Filipe"
+podpis = "Váš program"
+vysledek = 15
+
+print(f"""Mil{y_a} {osloveni},
+Váš výsledek je {vysledek}.
+S pozdravem,
+{podpis}.""")
+
+sablona = "Ahoj {jmeno}, tvé číslo je {cislo}."
+
+print(sablona.format(jmeno = "Filipe", cislo = 4))
+print(sablona.format(jmeno = "Lucie", cislo = 19))
+print(sablona.format(jmeno = "Milane", cislo = 10))
+
+vypis = "{} krát {} je {}"
+print(vypis.format(3,4,3*4))
